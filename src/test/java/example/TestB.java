3c465ddb1951dafe13fa1a4faac293ab6f8964c7
Alessandro Mecca <amecca@qmetric.co.uk>
2014-09-26 12:35:58 +0100

    Simplified Test classes

--- src/test/java/example/TestB.java
+++ src/test/java/example/TestB.java
@@ -2,15 +2,11 @@ package example;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 public class TestB
 {
     @Test
     public void test()
     {
-        System.out.println("=== RUN " + this.getClass().getSimpleName());
-        assertThat(true, is(true));
+        System.out.println("=== RUN " + this.getClass());
     }
 }
